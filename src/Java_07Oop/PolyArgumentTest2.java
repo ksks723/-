40a6ca860d8fe4698cc36ab23ae2152482434c8b
ksks723 <ksks723@gmail.com>
2020-11-04 13:09:23 +0900

    여러종류의 객체를 배열로다루기

--- src/Java_07Oop/PolyArgumentTest2.java
+++ src/Java_07Oop/PolyArgumentTest2.java
@@ -7,5 +7,16 @@ public class PolyArgumentTest2 {
 }
 
 class Pro{  //product 인데 PolyArgumentTest.java와 겹쳐서 이렇게 했음
+    int price; //제품의 가격
+    int bonusPoint; //제품구매 시 제공하는 보너스점수
 
+    Pro(int price){
+        this.price = price;
+        bonusPoint = (int)(price/10.0);
+    }
+
+    Pro(){  //기본생성자
+
+    }
 }
+
